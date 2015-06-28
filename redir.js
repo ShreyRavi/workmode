42e7dd17b2f52c4d246c54edd937c22d6f4bf290
stallamraju <shreyastallamraju@gmail.com>
2015-06-28 20:30:33 +0800

    added more websites
    Facebook, twitter, youtube, reddit, pinterest, vimeo, google plus,
    tumblr

--- redir.js
+++ redir.js
@@ -4,7 +4,43 @@ localStorage["activated"] = "false";
 chrome.tabs.onUpdated.addListener(function(id, info, tab){
     var bool = localStorage["activated"];
     if(bool.indexOf("true") > -1){
+        //fb
         if (tab.url.toLowerCase().indexOf("facebook.com/") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //tw
+        if (tab.url.toLowerCase().indexOf("twitter.com/") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //yt
+        if (tab.url.toLowerCase().indexOf("youtube.com/") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //xxx
+        if (tab.url.toLowerCase().indexOf("porn") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        if (tab.url.toLowerCase().indexOf("sex") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //re
+        if (tab.url.toLowerCase().indexOf("reddit.com/") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //pi
+        if (tab.url.toLowerCase().indexOf("pinterest.com/") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //vm
+        if (tab.url.toLowerCase().indexOf("vimeo.com/") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //gp
+        if (tab.url.toLowerCase().indexOf("plus.google") !== -1){
+            chrome.tabs.update(tab.id, {url: redir});
+        }
+        //tm
+        if (tab.url.toLowerCase().indexOf("tumblr.com/") !== -1){
             chrome.tabs.update(tab.id, {url: redir});
         }
 
